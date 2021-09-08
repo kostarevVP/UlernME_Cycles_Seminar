38ac0352a56e90115c2ae3aa769804351e1a938c
Kostariev Vadim <42378446+kostarevVP@users.noreply.github.com>
2021-09-08 19:13:35 +0300

    Delete Program (3).cs

--- Cycles_Seminar/Program.cs
+++ Cycles_Seminar/Program.cs
@@ -8,9 +8,12 @@ namespace Cycles_Seminar
         {
             while (true)
             {
-                Loops1();
-                Loops2();
-                Loops3();
+                //Loops1();
+                //Loops2();
+                //Loops3();
+                //Loops4();
+                Loops5();
+                Console.ReadKey();
             }
         }
         
@@ -66,5 +69,90 @@ namespace Cycles_Seminar
             Console.WriteLine(numericalSeries[position-1]);
         }
 
+        private static void Loops4()
+        {
+            Console.WriteLine("Loops4. В массиве чисел найдите самый длинный подмассив из одинаковых чисел.");
+            int[] array = { 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 0 };
+            var count = 0;
+            var point = 0;
+            var countTemp = 0;
+            var pointTemp = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] == array[i])
+                {
+                    countTemp++;
+                }
+                if (array[i - 1] != array[i] || i + 1 == array.Length)
+                {
+                    if (countTemp > count)
+                    {
+                        count = countTemp;
+                        if (i + 1 == array.Length && array[i - 1] == array[i])
+                            point = i + 1;
+                        else
+                            point = i;
+                    }
+                    countTemp = 0;
+                }
+
+            }
+            if (count <= 0)
+            {
+                Console.WriteLine("нет елементов масива которые повторяются подряд");
+            }
+            else if (count > 0)
+            {
+                var subArray = new int[count + 1];
+                Array.Copy(array, point - 1 - count, subArray, 0, count + 1);
+                Console.WriteLine($"В даном масиве начиная из {point - count} елемента идет наиболший подмасив из {count + 1} елементов с одинаковыми числами подряд");
+                foreach (int i in subArray)
+                    Console.Write($"{subArray[i]} ");
+            }
+        }
+
+        private static void Loops5()
+        {
+            Console.WriteLine("Loops5. Дана строка из символов '(' и ')'. Определить, является ли она корректным скобочным выражением. Определить максимальную глубину вложенности скобок.");
+            string str = "(()((()(()))()))";
+            Console.WriteLine(str);
+            string negativeAnswer = "Даное скобочное выражение не правильно";
+
+            if (String.IsNullOrEmpty(str))
+            {
+                Console.WriteLine($"строка пустая, нечего проверять{str.Length}");
+            }
+            else if (str.Length % 2 != 0)
+            {
+                Console.WriteLine(negativeAnswer);
+            }
+            else
+            {
+                var depthMax = str.Length / 2;
+                var depth = 0;
+                while (true)
+                {
+                    if (String.IsNullOrEmpty(str))
+                    {
+                        Console.WriteLine($"Даное скобочное выражение правильно и имеет максимальную глубину вложености скобок {depth}");
+                        break;
+                    }
+                    for (int i = 1; i < str.Length; i++)
+                    {
+                        if (str[i - 1] == '(' && str[i] == ')')
+                        {
+                            str = str.Substring(0, i - 1) + str.Substring(i + 1);
+                            //i = 1;
+                        }
+                    }
+                    depth++;
+                    if (depth > depthMax)
+                    {
+                        Console.WriteLine(negativeAnswer);
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
